3ccd38b4ce9daf31c1365871c04fd655b5a0bc17
Jonathan Dotore <jonathandotore@outlook.com>
2023-08-26 13:10:53 -0300

    Restarting building project

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const display = document.querySelector("#display")
-const ecqualButton = document.querySelector(".ecqual")
-const dotButton = document.querySelector(".dot")
-const numbersButton = document.querySelector(".nums")
-const operatorsButton= document.querySelector(".funcs")
-
